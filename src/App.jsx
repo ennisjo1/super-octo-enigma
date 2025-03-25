ce5c2d254745d884158a556e08d50a85b54b9d67
ennisjo <ennisjo1@msu.edu>
2025-03-25 18:07:18 +0000

    More changes than Strictily Required

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '.assets/panda.svg'
+import reactLogo from './assets/star.svg'
+import viteLogo from './assets/panda.svg'
 import './App.css'
 
 function App() {
@@ -9,30 +9,28 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
+      <a href="https://www.youtube.com/watch?v=ZN5PoW7_kdA" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://www.youtube.com/watch?v=ZN5PoW7_kdA" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+
+        <a href="https://www.youtube.com/watch?v=ZN5PoW7_kdA" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Welcome to JCE Week 9 Assignment, Click to Support</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setCount((count) => count + 1000)}>
+          Satisfaction Units = {count}
         </button>
         <p>
-          Proof of Concept; Making changes with a test
+          I'm glad to see your Satisfied
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        To support the channel feel free to hit that like and subscribe
       </p>
     </>
   )
